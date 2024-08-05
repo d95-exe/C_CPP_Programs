6fd9c26847d7aca80c44b4949a7203c956a82201
Dushyant Kumar <157031778+d95-exe@users.noreply.github.com>
2024-08-05 22:20:04 +0530

    Update palindromechecker_string.cpp

--- palindromechecker_string.cpp
+++ palindromechecker_string.cpp
@@ -1,22 +1,18 @@
 #include<iostream>
 using namespace std;
-bool checkPalindrome ( string s )
-{
-    int n = s.length();
-    for ( int i = 0; i <= n/2; i++ )
-    {
-        if ( s[i] != s[n-1-i] )
-            return false;
-    }
-    return true;
-}
 int main(){
-    string s;
-    cout<<"\nEnter a string to check if it's a palindrome:\n";
-    getline(cin, s);
-    if(checkPalindrome(s) == 1){
-        cout<<"\nEntered string is a palindrome.";
-    }else{
-        cout<<"\nEntered string is not a palindrome";
+    string word;
+    cout<<"\nPalindrome Checker\nEnter a word to check if it's a palindrome:\n";
+    cin>>word;
+    int size = word.size();
+    char drow[size];
+    for(int i=size-1; i>=0; i--){
+        drow[size - 1 - i] = word[i];
+    }
+    if(word == drow){
+        cout<<"\nThe entered string is a palindrome.";
+    }
+    else{
+        cout<<"\nThe entered string is not a palindrome.";
     }
 }
