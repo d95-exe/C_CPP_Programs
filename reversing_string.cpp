6435a240dd49e96ec4361883716ab06ab2e99835
Dushyant Kumar <157031778+d95-exe@users.noreply.github.com>
2024-08-08 22:29:15 +0530

    Update reversing_string.cpp

--- reversing_string.cpp
+++ reversing_string.cpp
@@ -1,13 +1,14 @@
 #include<iostream>
 using namespace std;
 int main(){
-    string input;
+    string input, op;
     int x, i;
-    cout<<"\nSTRING REVERSER\nGIVE MEh A WORD, AND I SHALL REVERSE IT\nEnter the word:\n";
+    cout<<"\nSTRING REVERSER\nGIVE ME A WORD, AND I SHALL REVERSE IT\nEnter the word:\n";
     cin>>input;
     x = input.length();
     cout<<"\nReversed, that's:\n";
     for(i=x; i>=0; i--){
-        cout<<input[i];
+        op += input[i];
     }
+    cout<<"\n"<<op;
 }
